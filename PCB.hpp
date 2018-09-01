3eca5a253a1c3c7b35180d5d009bf7c0db08b60a
Th3_M4j0r <spartan364@gmail.com>
2018-09-01 08:15:19 -0700

    Added Ostream operators and "#pragma once"

--- PCB.hpp
+++ PCB.hpp
@@ -1,5 +1,5 @@
 /*
-    Main C++ file
+    PCB header file
     Created By: Michael Lingo
     Created On: 8/31/18
     Last Update: 9/01/18
@@ -7,15 +7,44 @@
 
 */
 
+#pragma once //include only once
+
+
+#include <iostream>
+
 //using an enum because its more efficient than using a string
 enum class processState
 {
-    NEW, READY, RUNNING, WAITING, TERMINATED,
+    NEW,
+    READY,
+    RUNNING,
+    WAITING,
+    TERMINATED
 };
-
 
 struct ProcessControlBlock
 {
-    processState state;
-    unsigned ID;
+    processState state; //the current state of the process
+    unsigned ID;        //the ID of the process
+    unsigned priority;  //the priority of the process
 };
+
+std::ostream& operator <<(std::ostream& os, processState state)
+{
+    switch (state)
+    {
+        case processState::NEW       : os << "NEW"; break;
+        case processState::READY     : os << "READY"; break;
+        case processState::RUNNING   : os << "RUNNING"; break;
+        case processState::TERMINATED: os << "TERMINATED"; break;
+        case processState::WAITING   : os << "WAITING"; break;
+        default                      : os.setstate(std::ios_base::failbit);
+    }
+    return os;
+}
+
+std::ostream& operator <<(std::ostream& os, const ProcessControlBlock& process)
+{
+    os << process.ID << '\t' << process.state << '\t' << process.priority;
+    return os;
+}
