325d589322a63c676dde24b0e9229fcec391f6b0
Th3_M4j0r <spartan364@gmail.com>
2018-09-03 21:15:53 -0700

    Reworked PCB_Table be much closer to a complexity of O(1)

--- PCB_Table.cpp
+++ PCB_Table.cpp
@@ -12,13 +12,21 @@
 PCB_Table::PCB_Table()
 {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    keyVector.reserve(50);
     rand = std::default_random_engine(seed); //seed the random engine with system time
 }
 
 void PCB_Table::addNewPCB(ProcessControlBlock &process)
 {
-    auto ret = ProcessMap.insert(std::pair<PCB_ID_TYPE, ProcessControlBlock &>(process.ID, process));
-    if (ret.second == false) //was the insert unsuccessful?
+     if (ProcessMap.count(process.ID) != 0) //Is a process with this ID already in the map?
+    {
+        throw InsertFailedException();
+    }
+    keyVector.push_back(process.ID);
+
+    PCBKeyStruct toInsert = {keyVector.size() -1, process};
+    auto ret = ProcessMap.insert(std::pair<PCB_ID_TYPE, PCBKeyStruct>(process.ID, toInsert));
+    if (ret.second == false) //was the insert unsuccessful for some reason?
     {
         throw InsertFailedException();
     }
@@ -30,7 +38,7 @@ ProcessControlBlock &PCB_Table::getPCB(PCB_ID_TYPE ID)
     {
         throw ProcessNotFoundException();
     }
-    return ProcessMap.at(ID);
+    return ProcessMap.at(ID).block;
 }
 
 ProcessControlBlock &PCB_Table::removePCB(PCB_ID_TYPE ID)
@@ -39,30 +47,30 @@ ProcessControlBlock &PCB_Table::removePCB(PCB_ID_TYPE ID)
     {
         throw ProcessNotFoundException();
     }
-    ProcessControlBlock removedBlock = ProcessMap.at(ID);
+    PCBKeyStruct removedBlock = ProcessMap.at(ID);
+    if(ProcessMap.size() > 1) //no point in doing this if there is only one item in the map
+    {
+        unsigned toSwap = removedBlock.processVectorIndex;
+        ProcessMap.at(keyVector[keyVector.size() - 1]).processVectorIndex = toSwap;
+        keyVector[toSwap] = keyVector[keyVector.size() - 1]; //move toSwap to the last element
+    }
+    keyVector.pop_back(); //remove the last element from the vector
     ProcessMap.erase(ID);
-    return removedBlock;
+    return removedBlock.block;
 }
 
-ProcessControlBlock &PCB_Table::getRandomPCB()
+ProcessControlBlock &PCB_Table::removeRandomPCB()
 {
     if(ProcessMap.empty())
     {
         throw ProcessNotFoundException();
     }
-    unsigned count = rand() % ProcessMap.size();
-    std::map<PCB_ID_TYPE, ProcessControlBlock &>::const_iterator it = ProcessMap.cbegin();
-    for(int i = 0; i < count; i++)
-    {
-        it++; //cannot directly add count to it since it is an iterator
-    }
-    PCB_ID_TYPE key = it->first;
-    ProcessControlBlock removedBlock = it->second;
-    ProcessMap.erase(key);
-    return removedBlock;
+    unsigned pos = rand() % ProcessMap.size();
+    return removePCB(keyVector[pos]);
 }
 
 void PCB_Table::clear()
 {
     ProcessMap.clear();
+    keyVector.clear();
 }
--- PCB_Table.hpp
+++ PCB_Table.hpp
@@ -9,13 +9,20 @@
 #pragma once //include only once
 
 #include "PCB.hpp"
-#include <map> //storing the table in an unordered map
-#include <utility>       //for std::pair
+#include <unordered_map> //storing the table in an unordered map
+#include <vector>
+#include <utility> //for std::pair
 #include <stdexcept>
 #include <random>
 #include <chrono>
 
 //stores refrences to process control blocks
+
+struct PCBKeyStruct //a helper struct to allow removing the key from the key vector easier
+{
+  unsigned processVectorIndex;
+  ProcessControlBlock &block;
+};
 
 class InsertFailedException : public std::exception
 {
@@ -38,14 +45,15 @@ public:
 class PCB_Table
 {
 protected:
-  std::map<PCB_ID_TYPE, ProcessControlBlock &> ProcessMap;
+  std::unordered_map<PCB_ID_TYPE, PCBKeyStruct> ProcessMap;
   std::default_random_engine rand;
+  std::vector<PCB_ID_TYPE> keyVector;
 
 public:
   PCB_Table();
-  void addNewPCB(ProcessControlBlock &process);       //stores a refrence to a process control block, throws exception if store unsuccessful
-  ProcessControlBlock &getPCB(PCB_ID_TYPE processID); //returns a refrence to a process control block, throws exception if not found
+  void addNewPCB(ProcessControlBlock &process);          //stores a refrence to a process control block, throws exception if store unsuccessful
+  ProcessControlBlock &getPCB(PCB_ID_TYPE processID);    //returns a refrence to a process control block, throws exception if not found
   ProcessControlBlock &removePCB(PCB_ID_TYPE processID); //removes a specific PCB from the table
-  ProcessControlBlock &getRandomPCB(); //removes and returns a random PCB from the table, throws an exception if there aren't any to return
-  void clear();                                       //empties the table
+  ProcessControlBlock &removeRandomPCB();                   //removes and returns a random PCB from the table, throws an exception if there aren't any to return
+  void clear();                                          //empties the table
 };
