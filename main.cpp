89c1f21300226cc647ea206c43603c0af6a042eb
Th3_M4j0r <spartan364@gmail.com>
2018-09-04 12:54:42 -0700

    different unit test

--- main.cpp
+++ main.cpp
@@ -15,16 +15,16 @@ int main()
     PCB_Table table;
     try
     {
-        for(unsigned i = 1; i < 1'000'000; i++)
+        for(unsigned i = 1; i < 50; i++)
         {
             table.addNewPCB(createPCB(processState::NEW, i, i % 49 + 1));
         }
-        table.getPCB(20).state = processState::WAITING;
-        std::cout << sizeof(table) << std::endl;
+        auto block = table.getPCB(20);
+        block.state = processState::RUNNING;
         while(table.size() != 0)
         {
             auto block = table.removeRandomPCB();
-            //std::cout << block << std::endl;
+            std::cout << block << std::endl;
         }
     }
     catch(InsertFailedException e)
