a9d82cef9a3a576968ab639a875723f7a0040fe9
ShaneLaskowski <lasko002@cougars.csusm.edu>
2018-09-07 12:16:58 -0700

    Pushing the PCBqueue.hpp i didn't finish it but i will by today and plan on implementing the PCBqueue class by end of weekend.

---
+++ PCBqueue.hpp
@@ -0,0 +1,10 @@
+/*
+Purpose: To define the queue class for the priority queue class that uses PCB objects
+Description: delclares the PCBqueue class and its data/function members
+Created By: Shane Laskowski
+Creation Date: 9/7/2018
+Last Modifier: Shane Laskowski
+Last Modification Date: 9/7/2018
+*/
+
+#include<stufffffffff>
