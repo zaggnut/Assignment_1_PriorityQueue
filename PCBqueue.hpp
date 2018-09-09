b82297edcebbfcd0d4d7d6e093847710ca6a5dcb
ShaneLaskowski <lasko002@cougars.csusm.edu>
2018-09-08 20:00:17 -0700

    Now this will be pushed to the proper branch (not main branch).

---
+++ PCBqueue.hpp
@@ -0,0 +1,28 @@
+/*
+Purpose: Define the class PCBqueue to be used in the PCBpriorityQueue class. 
+		 This class is a queue data structure using a doubly linked list in std library.
+		 The content of this queue is made of ProcessControlBlock objects.
+Creation Date: 9/8/2018
+
+*/
+#ifndef PCBqueue
+#define PCBqueue
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+#include "PCB.hpp"
+
+class PCBqueue
+{
+
+protected:
+
+
+public:
+
+
+};
+
+#endif //PCBqueue.hpp
