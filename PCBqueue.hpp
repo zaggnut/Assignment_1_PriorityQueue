150dc904509970b5076b8e71d1458afebfe7a96c
ShaneLaskowski <lasko002@cougars.csusm.edu>
2018-09-08 22:28:10 -0700

    Declared function members and delcared data member(s).  added some in code comments.

--- PCBqueue.hpp
+++ PCBqueue.hpp
@@ -3,12 +3,13 @@ Purpose: Define the class PCBqueue to be used in the PCBpriorityQueue class.
 		 This class is a queue data structure using a doubly linked list in std library.
 		 The content of this queue is made of ProcessControlBlock objects.
 Creation Date: 9/8/2018
-
+Created By: Shane Laskowski
+Date Of Last Modifcation: 9/8/2018
+Last Modification By: Shane Laskowski
 */
 #ifndef PCBqueue
 #define PCBqueue
 
-#include <iostream>
 #include <list>
 using namespace std;
 
@@ -16,13 +17,14 @@ using namespace std;
 
 class PCBqueue
 {
-
 protected:
-
-
+	list<ProcessControlBlock> queueList; //doubly linked list in std library, used to hold PCB objects in an ordered fashion
 public:
-
-
+	void enQueue(ProcessControlBlock PCBtoBeAdded); //adds the PCB object to rear of the queue
+	ProcessControlBlock deQueue(); //removes the PCB object from the front of the queue
+	void printQueue(); //prints out all PCB objects of the queue
+	void printLeadingPCB(); //prints out the PCB object in the front of the queue
+	bool isEmpty(); //returns true if the queue is empty, else returns false
 };
 
 #endif //PCBqueue.hpp
