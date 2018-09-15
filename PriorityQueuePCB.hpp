febc3d77264e08ce997d62ef4143de0873d671b5
ShaneLaskowski <lasko002@cougars.csusm.edu>
2018-09-15 08:22:11 -0700

    a new branch just for the simple re-implementation of priorityqueue class with map and not a vector.

--- PriorityQueuePCB.hpp
+++ PriorityQueuePCB.hpp
@@ -9,6 +9,7 @@ Last Modification By:  Shane Laskowski
 #ifndef PriorityQueuePCB
 #define PriorityQueuePCB
 
+#include <map>
 #include "PCBqueue.hpp"
 #include "PCB.hpp"
 
@@ -20,18 +21,14 @@ class PriorityQueuePCB
 {
 protected:
 
-	//vector of PCB queue objects.  Each queue should represent a set of PCBs with same priority level.
-	//This vector is declared, but has no PCB queue objects (its empty), should have a custom made construcutor to initialize it properly.
-	vector<PCBqueue> PriorityQueue; //perhaps use a tree and not a vector ?
-
+	
+	std::map<int, PCBqueue> PriorityQueue; //the key of the map is for priority level, the mapped value is a PCBqueue object 
 	int PCBptrCount; //used to count number of PCB items are stored in the priorityqueue.  usefull in its Size() and isEmpty() function
 
 public:
 
 	~PriorityQueuePCB();
 	PriorityQueuePCB();
-	PriorityQueuePCB(int TotalPriorityLevels); // a constructor that takes an argument that will expand the memeory occupied by the empty PriorityQueue vector
-											   // this helps keep the vector from constantly copying and placing its content in new memory spaces for each size change
 	
 	void addPCB(PCB_ID_TYPE ID); //adds a PCB shared ptr to the PriorityQueue (vector) based on the ID number of the specific PCB
 	std::shared_ptr<ProcessControlBlock> removePCB();  //removes a PCB shared ptr from the priority queue and returns it
